3124ab3a5f354f515fdac7a9747168a2d26bd719
Adithya Sreyaj <adi.sreyaj@gmail.com>
2019-10-29 17:21:37 +0530

    update ts, added airbnb tslint rules

--- src/api/controllers/main.controller.ts
+++ src/api/controllers/main.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from 'express';
 export class MainController {
-	public static async getHealthInfo(req: Request, res: Response) {
-		res
-			.json({
-				status: 'up'
-			})
-			.status(200);
-	}
+  public static async getHealthInfo(req: Request, res: Response) {
+    res
+      .json({
+        status: 'up',
+      })
+      .status(200);
+  }
 }
--- src/api/routes/main.route.ts
+++ src/api/routes/main.route.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { MainController } from '../controllers/main.controller';
 
 export class MainRoute {
-	public static register(app: express.Application) {
-		app.route('/api/v1/health/').get(MainController.getHealthInfo);
-	}
+  public static register(app: express.Application) {
+    app.route('/api/v1/health/').get(MainController.getHealthInfo);
+  }
 }

e2701de145f52d42f6b7b426821a707a2a50adf6
Adithya Sreyaj <adi.sreyaj@gmail.com>
2019-10-30 22:01:44 +0530

    Bug fixing in test file

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,20 +1,20 @@
 import { expect } from 'chai';
 import express from 'express';
-import request from 'supertest';
+import supertest from 'supertest';
 import { App } from '../src/app';
 
 let app: express.Application;
 let appInstance: App;
 
 beforeAll(async () => {
-	appInstance = new App();
- app = appInstance.app;
+  appInstance = new App();
+  app = appInstance.app;
 });
 
 describe('Test GET /health Api', () => {
-	it('It should return status as up', async () => {
-		const response = await request(app).get('/api/v1/health');
-		expect(response.status).to.equal(200);
-		expect(response.text).to.equal('{"status":"up"}');
-	});
+  it('It should return status as up', async () => {
+    const response = await supertest(app).get('/api/v1/health');
+    expect(response.status).to.equal(200);
+    expect(response.text).to.equal('{"status":"up"}');
+  });
 });
